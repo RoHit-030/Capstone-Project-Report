532ab617b4de845541152616f4a74fef825c6310
RoHit-030 <154572541+RoHit-030@users.noreply.github.com>
2025-11-09 15:03:13 +0530

    Add files via upload

---
+++ build/CMakeFiles/filex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/filex.dir/main.cpp.o"
+  "CMakeFiles/filex.dir/main.cpp.o.d"
+  "filex"
+  "filex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/filex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/filex.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for filex.
+# This may be replaced when dependencies are built.
